9732f8145481b6e50af7aaa05db33db3a450c9d1
Mariusz Notariusz <mariusz@notariusz.pl>
2015-05-28 09:04:47 +0200

    Pierwszy commit

---
+++ com.mariusz/src/main/java/com/mariusz/App.java
@@ -0,0 +1,13 @@
+package com.mariusz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
